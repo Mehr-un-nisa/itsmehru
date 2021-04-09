8532c0d69917a7685bde4db794da1fc8dcf8d6bc
Mehru <mehrunnisa533@gmail.com>
2021-04-09 23:37:25 +0500

    Js fuctnion implementation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello");
+var database = {
+  username: "Mehru",
+  password: "2345",
+};
+
+var usernameprompt = prompt("what is your name");
+var userpasswordprompt = prompt("what is your passsword");
+
+function signin(name, pass) {
+  if (name == database.username && pass == database.password) {
+    console.log("True username is", usernameprompt);
+  } else {
+    console.log("False username is", usernameprompt);
+  }
+}
+console.log(signin(usernameprompt, userpasswordprompt));
